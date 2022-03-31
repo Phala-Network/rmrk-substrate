12cf173e22c53a47551b1260815b1ba567d9c53c
hashwarlock <hashwarlock@protonmail.com>
2022-03-30 19:00:09 -0500

    Initial benchmarking

--- pallets/phala-world/src/benchmarking.rs
+++ pallets/phala-world/src/benchmarking.rs
@@ -5,16 +5,24 @@ use super::*;
 #[allow(unused)]
 use crate::Pallet as PhalaWorld;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
-use frame_system::RawOrigin;
+use frame_system::Origin;
+
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	let events = frame_system::Pallet::<T>::events();
+	let system_event: <T as frame_system::Config>::Event = generic_event.into();
+	// compare to the last event record
+	let frame_system::EventRecord { event, .. } = &events[events.len() - 1];
+	assert_eq!(event, &system_event);
+}
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
+	set_overlord {
 		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
+		T::Currency::make_free_balance_be(&caller, DepositBalanceOf::<T>::max_value());
+	}: _(Origin::root(), caller)
 	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+		assert_last_event::<T>(Event::OverlordChanged { old_overlord: None }.into());
 	}
 
-	impl_benchmark_test_suite!(PhalaWorld, crate::mock::new_test_ext(), crate::mock::Test);
+	impl_benchmark_test_suite!(PhalaWorld, crate::mock::ExtBuilder::default().build(None), crate::mock::Test);
 }
