5ae3b1c2fb345e3e5235699ed7642082cb1b0b71
hashwarlock <hashwarlock@protonmail.com>
2022-05-12 16:18:59 -0500

    Update init and create populate dummy data script

--- scripts/pw/init.js
+++ scripts/pw/init.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const BN = require('bn.js');
 const sleep = require('p-sleep');
 const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
 const { stringToU8a, u8aToHex } = require('@polkadot/util');
@@ -7,6 +8,11 @@ const rootPrivkey = process.env.ROOT_PRIVKEY;
 const userPrivkey = process.env.USER_PRIVKEY;
 const overlordPrivkey = process.env.OVERLOAD_PRIVKEY;
 const endpoint = process.env.ENDPOINT;
+
+const bnUnit = new BN(1e12);
+function token(n) {
+    return new BN(n).mul(bnUnit);
+}
 
 async function main() {
     const wsProvider = new WsProvider(endpoint);
@@ -24,15 +30,43 @@ async function main() {
             }
         }
     });
+
+    async function checkUntil(async_fn, timeout) {
+        const t0 = new Date().getTime();
+        while (true) {
+            if (await async_fn()) {
+                return true;
+            }
+            const t = new Date().getTime();
+            if (t - t0 >= timeout) {
+                return false;
+            }
+            await sleep(100);
+        }
+    }
+
+    async function getNonce(address) {
+        const info = await api.query.system.account(address);
+        return info.nonce.toNumber();
+    }
+    async function waitTxAccepted(account, nonce) {
+        await checkUntil(async () => {
+            return await getNonce(account) == nonce + 1;
+        });
+    }
+
     const keyring = new Keyring({ type: 'sr25519' });
     // status types
     const claimSpirits = api.createType('StatusType', 'ClaimSpirits');
     const root = keyring.addFromUri(rootPrivkey);
     const user = keyring.addFromUri(userPrivkey);
     const overlord = keyring.addFromUri(overlordPrivkey);
+    let nonceRoot = await getNonce(root.address);
 
     // prep
     {
+        await api.tx.balances.transfer(overlord.address, token(1_000_000)).signAndSend(root, {nonce: nonceRoot++});
+        await waitTxAccepted(root.address, nonceRoot - 1);
         await api.tx.sudo.sudo(
             api.tx.pwNftSale.setOverlord(overlord.address)
         ).signAndSend(root, {nonce: -1});
---
+++ scripts/pw/populate-dummy-data.js
@@ -0,0 +1,188 @@
+require('dotenv').config();
+const sleep = require('p-sleep');
+const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+const { stringToU8a, u8aToHex } = require('@polkadot/util');
+
+const rootPrivkey = process.env.ROOT_PRIVKEY;
+const userPrivkey = process.env.USER_PRIVKEY;
+const overlordPrivkey = process.env.OVERLOAD_PRIVKEY;
+const ferdiePrivkey = process.env.FERDIE_PRIVKEY;
+const charliePrivkey = process.env.CHARLIE_PRIVKEY;
+const davidPrivkey = process.env.DAVID_PRIVKEY;
+const evePrivkey = process.env.EVE_PRIVKEY;
+const endpoint = process.env.ENDPOINT;
+
+async function main() {
+    const wsProvider = new WsProvider(endpoint);
+    const api = await ApiPromise.create({
+        provider: wsProvider,
+        types: {
+            RaceType: {
+                _enum: ['Cyborg', 'AISpectre', 'XGene', 'Pandroid']
+            },
+            CareerType: {
+                _enum: ['HardwareDruid', 'RoboWarrior', 'TradeNegotiator', 'HackerWizard', 'Web3Monk']
+            },
+            StatusType: {
+                _enum: ['ClaimSpirits', 'PurchaseRareOriginOfShells', 'PurchasePrimeOriginOfShells', 'PreorderOriginOfShells']
+            },
+            OriginOfShellType: {
+                _enum: ['Prime', 'Magic', 'Legendary']
+            },
+            PreorderStatus: {
+                _enum: ['Pending', 'Chosen', 'NotChosen']
+            },
+            PreorderInfo: {
+                owner: "AccountId",
+                race: "RaceType",
+                career: "CareerType",
+                metadata: "BoundedString",
+                preorder_status: "PreorderStatus",
+            },
+            NftSaleInfo: {
+                race_count: "u32",
+                race_for_sale_count: "u32",
+                race_giveaway_count: "u32",
+                race_reserved_count: "u32",
+            },
+            PurposeType: {
+                _enum: ['RedeemSpirit', 'BuyPrimeOriginOfShells']
+            },
+            OverlordMessage: {
+                account: "AccountId",
+                purpose: "PurposeType",
+            },
+        }
+    });
+
+    async function checkUntil(async_fn, timeout) {
+        const t0 = new Date().getTime();
+        while (true) {
+            if (await async_fn()) {
+                return true;
+            }
+            const t = new Date().getTime();
+            if (t - t0 >= timeout) {
+                return false;
+            }
+            await sleep(100);
+        }
+    }
+
+    async function getNonce(address) {
+        const info = await api.query.system.account(address);
+        return info.nonce.toNumber();
+    }
+    async function waitTxAccepted(account, nonce) {
+        await checkUntil(async () => {
+            return await getNonce(account) == nonce + 1;
+        });
+    }
+
+    const keyring = new Keyring({type: 'sr25519'});
+
+    const alice = keyring.addFromUri(rootPrivkey);
+    const bob = keyring.addFromUri(userPrivkey);
+    const ferdie = keyring.addFromUri(ferdiePrivkey);
+    const overlord = keyring.addFromUri(overlordPrivkey);
+    const charlie = keyring.addFromUri(charliePrivkey);
+    const david = keyring.addFromUri(davidPrivkey);
+    const eve = keyring.addFromUri(evePrivkey);
+    let nonceAlice = await getNonce(alice.address);
+    let nonceBob = await getNonce(bob.address);
+    let nonceCharlie = await getNonce(charlie.address);
+    let nonceDavid = await getNonce(david.address);
+    let nonceEve = await getNonce(eve.address);
+    let nonceFerdie = await getNonce(ferdie.address);
+
+    // Claim Spirits
+    {
+        console.log(`Claiming Spirits...`);
+        const purposeType = api.createType('PurposeType', 'RedeemSpirit');
+        const overlordMessage = api.createType('OverlordMessage', {'account': ferdie.address, 'purpose': purposeType});
+        const metadataSig = overlord.sign(overlordMessage.toU8a());
+        await api.tx.pwNftSale.claimSpirit().signAndSend(alice);
+        await api.tx.pwNftSale.claimSpirit().signAndSend(bob);
+        await api.tx.pwNftSale.claimSpirit().signAndSend(charlie);
+        await api.tx.pwNftSale.claimSpirit().signAndSend(david);
+        await api.tx.pwNftSale.claimSpirit().signAndSend(eve);
+        await api.tx.pwNftSale.redeemSpirit(metadataSig).signAndSend(ferdie);
+        console.log(`Claiming Spirits Done.`);
+    }
+
+    // Buy Rare Origin of Shell NFTs (alice, bob, charlie, david)
+    {
+        console.log(`Purchase Rare Origin of Shells...`);
+        await api.tx.pwNftSale.setStatusType(true, 'PurchaseRareOriginOfShells')
+            .signAndSend(overlord);
+        await api.tx.pwNftSale.buyRareOriginOfShell('Legendary', 'Cyborg', 'HackerWizard')
+            .signAndSend(bob);
+        await api.tx.pwNftSale.buyRareOriginOfShell('Legendary', 'AISpectre', 'Web3Monk')
+            .signAndSend(alice);
+        await api.tx.pwNftSale.buyRareOriginOfShell('Magic', 'Pandroid', 'RoboWarrior')
+            .signAndSend(charlie);
+        await api.tx.pwNftSale.buyRareOriginOfShell('Magic', 'XGene', 'TradeNegotiator')
+            .signAndSend(david);
+        console.log(`Purchase Rare Origin of Shells Done.`);
+    }
+
+    // Buy Prime Origin of Shell NFTs Whitelist (eve & ferdie)
+    {
+        console.log(`Purchase Prime Origin of Shells Whitelist...`);
+        await api.tx.pwNftSale.setStatusType(true, 'PurchasePrimeOriginOfShells')
+            .signAndSend(overlord);
+        const purposeType = api.createType('PurposeType', 'BuyPrimeOriginOfShells');
+        const ferdieWlMessage = api.createType('OverlordMessage', {'account': ferdie.address, 'purpose': purposeType});
+        const eveWlMessage = api.createType('OverlordMessage', {'account': eve.address, 'purpose': purposeType});
+        const ferdieWlSig = overlord.sign(ferdieWlMessage.toU8a());
+        const eveWlSig = overlord.sign(eveWlMessage.toU8a());
+        await api.tx.pwNftSale.buyPrimeOriginOfShell(ferdieWlSig, 'Cyborg', 'HackerWizard')
+            .signAndSend(ferdie);
+        await api.tx.pwNftSale.buyPrimeOriginOfShell(eveWlSig, 'XGene', 'Web3Monk')
+            .signAndSend(eve);
+        console.log(`Purchase Prime Origin of Shells Whitelist Done`);
+    }
+
+    // Enable Preorders of Prime Origin of Shell NFTs
+    {
+        console.log(`Preorder Prime Origin of Shells Non-Whitelist...`);
+        await api.tx.pwNftSale.setStatusType(true, 'PreorderOriginOfShells')
+           .signAndSend(overlord);
+        await api.tx.pwNftSale.preorderOriginOfShell('Pandroid', 'HackerWizard')
+           .signAndSend(ferdie, { nonce: nonceFerdie++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('AISpectre', 'Web3Monk')
+            .signAndSend(alice, { nonce: nonceAlice++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('Pandroid', 'RoboWarrior')
+            .signAndSend(bob, { nonce: nonceBob++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('Cyborg', 'Web3Monk')
+            .signAndSend(charlie, { nonce: nonceCharlie++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('Cyborg', 'HardwareDruid')
+            .signAndSend(david, { nonce: nonceDavid++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('XGene', 'TradeNegotiator')
+            .signAndSend(eve, { nonce: nonceEve++ } );
+        // Wait Tx Accepted
+        await waitTxAccepted(ferdie.address, nonceFerdie - 1);
+        await api.tx.pwNftSale.preorderOriginOfShell('XGene', 'HackerWizard')
+            .signAndSend(ferdie, { nonce: nonceFerdie++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('AISpectre', 'HackerWizard')
+            .signAndSend(alice, { nonce: nonceAlice++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('XGene', 'RoboWarrior')
+            .signAndSend(bob, { nonce: nonceBob++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('Pandroid', 'HardwareDruid')
+            .signAndSend(charlie, { nonce: nonceCharlie++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('AISpectre', 'TradeNegotiator')
+            .signAndSend(david, { nonce: nonceDavid++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('Pandroid', 'Web3Monk')
+            .signAndSend(eve, { nonce: nonceEve++ } );
+        // Wait for TX accepted
+        await waitTxAccepted(ferdie.address, nonceFerdie - 1);
+        await api.tx.pwNftSale.preorderOriginOfShell('Cyborg', 'HackerWizard')
+            .signAndSend(ferdie, { nonce: nonceFerdie++ } );
+        await api.tx.pwNftSale.preorderOriginOfShell('Pandroid', 'TradeNegotiator')
+            .signAndSend(eve, { nonce: nonceEve++ } );
+        console.log(`Preorder Prime Origin of Shells Non-Whitelist Done`);
+    }
+
+}
+
+main().catch(console.error).finally(() => process.exit());
